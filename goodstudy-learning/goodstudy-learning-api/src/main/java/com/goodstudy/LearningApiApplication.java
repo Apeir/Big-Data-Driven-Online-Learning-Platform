e9ab87b9d03977bea43c40f5a9b049a5983c659a
Apeir <89534239+Apeir@users.noreply.github.com>
2025-05-28 17:26:06 +0800

    update swagger

--- goodstudy-learning/goodstudy-learning-api/src/main/java/com/goodstudy/LearningApiApplication.java
+++ goodstudy-learning/goodstudy-learning-api/src/main/java/com/goodstudy/LearningApiApplication.java
@@ -1,11 +1,13 @@
 package com.goodstudy;
 
+import com.spring4all.swagger.EnableSwagger2Doc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients(basePackages={"com.goodstudy.*.feignclient"})
 @SpringBootApplication
+@EnableSwagger2Doc
 public class LearningApiApplication {
 
     public static void main(String[] args) {
